3361b6b8a25ff5a0ea1d3ff512a7839fd35d9a4a
emelda <emelda.perez@moringaschool.com>
2021-09-12 23:31:59 +0300

    Add js slideDown method to display or show the p element on the 'what we do section.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    $
-})
+    $("#designicon").click(function(){
+        $("#designicon").slideDown('1000').hide('500');
+        $("#whatwedo1").show('1000');
+    });
+});   
