e417955f567fd6615f632b3a61cd046e6883b053
emelda <emelda.perez@moringaschool.com>
2021-09-12 23:34:15 +0300

    Add js slideUP method that hides the p element in the 'what we do' section and the js slideDown method to display the design icob when the design p element is clicked.

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
         $("#designicon").slideDown('1000').hide('500');
         $("#whatwedo1").show('1000');
     });
+    $("#whatwedo1").click(function () {
+        $("#whatwedo1").slideUp('1000');
+        $("#designicon").slideDown('1000');
+    });
+    
 });   
